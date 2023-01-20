ef3543a943d31fffaa47f343499e584834890c73
Khen Cahyo <71414962+KhenCahyo13@users.noreply.github.com>
2023-01-20 04:55:10 +0000

    config verification email

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,17 @@
 const Database = require('../config/Database')
 const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
+const crypto = require('crypto')
+
+let testAccount = nodemailer.createTestAccount();
+// Nodemailer verification
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "khencahyo02@gmail.com", // generated ethereal user
+    pass: 'qojyryonglfatjjr',
+  }
+});
 
 const UsersController = {
   createUser: async (request, response) => {
@@ -7,6 +19,8 @@ const UsersController = {
       // Get Request Value
       const { fullname, email, password, confPassword } = request.body
       const picture = request.file.filename
+      const verification_token = crypto.randomBytes(16).toString('hex')
+      const exp_time = Date.now() + (60 * 60 * 24 * 1000) // 24 Hours
       // Create URL Picture
       let pictureURL = request.protocol + "://" + request.get("host") + "/profil_image/" + request.file.filename
 
@@ -18,13 +32,47 @@ const UsersController = {
       if (password !== confPassword) {
         response.status(400).json({ msg: "Password and Password Confirmation must be the same" })
       } else {
-        const query = 'INSERT INTO users (fullname, email, password, picture, url_picture) VALUES (?, ?, ?, ?, ?)'
-        const [result] = await Database.query(query, [fullname, email, hashedPassword, picture, pictureURL])
-        response.status(201).json({ msg: "Registration successfully" })
+        const query = 'INSERT INTO users (fullname, email, password, picture, url_picture, verification_token, exp_time) VALUES (?, ?, ?, ?, ?, ?, ?)'
+        const [result] = await Database.query(query, [fullname, email, hashedPassword, picture, pictureURL, verification_token, exp_time])
+        // Send Email Verification to User
+        let mailOptions = {
+          from: '"Verify your email address" <khencahyo02@gmail.com>',
+          to: email,
+          subject: 'Todo App -Email Verification',
+          html: `<h2>Hi ${fullname}, thanks for registering in Todo App</h2>
+                 <h4>Please verify your email to continue in app</h4>
+                 <a href="https://${request.headers.host}/todoapp/users/${verification_token}">Verify your email in here</a>
+          `
+        }
+
+        // Send Email
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error)
+          } else {
+            response.status(201).json({ msg: "Verification email is send to your gmail account" })
+          }
+        })
+
+        // response.status(201).json({ msg: "Registration successfully" })
       }
     } catch (error) {
       console.log(error)
       response.status(400).json({ msg: error })
+    }
+  },
+
+  verifyEmail: async (request, response) => {
+    try {
+      const { token } = request.params
+      const verification_token = null
+      const is_verified = true
+
+      const query = "UPDATE users SET is_verified = ?, verification_token = ? WHERE verification_token = ?"
+      const [results] = await Database.query(query, [is_verified, verification_token, token])
+      response.status(201).json({ msg: "Email has been verified" })
+    } catch (error) {
+      console.log(error)
     }
   },
 
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -7,21 +7,30 @@ const fs = require('fs')
 const UsersController = require('../controllers/UsersController')
 
 const storage = multer.diskStorage({
-    destination: (request, file, cb) => {
-        cb(null, './src/profil_image')
-    },
-    filename: (request, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
+  destination: (request, file, cb) => {
+    cb(null, './src/profil_image')
+  },
+  filename: (request, file, cb) => {
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+  }
 })
 
 const upload = multer({
-    storage: storage
+  storage: storage,
+  fileFilter: (request, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/jpg' || file.mimetype == 'image/png') {
+      cb(null, true)
+    } else {
+      cb(null, false)
+      return cb(new Error('File image only jpeg, jpg, and png'))
+    }
+  }
 })
 
 uRouter.post('/', upload.single('picture'), UsersController.createUser)
 uRouter.get('/', UsersController.getAllUser)
 uRouter.get('/:id', UsersController.getUserById)
-uRouter.put('/:id_user', UsersController.updateUser)
+uRouter.put('/:token', UsersController.verifyEmail)
+uRouter.put('/:id_user', upload.single('picture'), UsersController.updateUser)
 
 module.exports = uRouter
